b4ad312625dc6cb5bde3cf3787cd52f33bbc638b
ankushpatil0125 <155605884+ankushpatil0125@users.noreply.github.com>
2024-04-07 13:34:15 +0530

    Change Entire Project Structure + Signout Implementd + Working Code
    Change Entire Project Structure + Signout Implementd + Working Code

--- HealthWorker/App.js
+++ HealthWorker/App.js
@@ -1,161 +1,35 @@
-import "react-native-gesture-handler";
-import React, { useEffect, useState } from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { Button, StyleSheet, Text, View } from "react-native";
-import NetInfo from "@react-native-community/netinfo";
-import Toast from "react-native-toast-message";
-import CreateService from "./Services/DatabaseServices/CreateService";
-import HomeScreen from "./Screens/HomeScreen";
-import RegisterPatientScreen from "./Screens/RegisterPatientScreen";
-import PatientDetailsScreen from "./Screens/PatientDetailsScreen";
-import QuestionnaireScreen from "./Screens/QuestionnaireScreen";
-import MedicalDetails from "./Screens/MedicalDetails";
-import Preview from "./Screens/Preview";
-import db from "./Services/DatabaseServices/DatabaseServiceInit";
-import DropService from "./Services/DatabaseServices/DropService";
-import PatientContext, { PatientProvider } from "./Context/PatientContext";
-import LoginScreen from "./Screens/LoginScreen";
-import MissedFollowUpsScreen from "./Screens/MissedFollowUpsScreen";
-import { LanguageProvider } from "./Context/LanguageProvider";
-import MainDrawerNavigator from "./navigation/MainDrawerNavigator";
-import FetchDataService from "./Services/FetchDataService";
-import RegisterPatientService from "./Services/RegisterPatientService";
-
-
-// const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
-
-const ForwardedToast = React.forwardRef((props, ref) => (
-  <Toast ref={ref} {...props} />
-));
-
-const checkNetworkConnectivity = async () => {
-  const state = await NetInfo.fetch();
-  return state.isConnected;
-};
-
-// Define a function to render the home stack
-export const HomeStack = () => (
-  <PatientProvider>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="RegisterPatientScreen"
-        component={RegisterPatientScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="PatientDetailsScreen"
-        component={PatientDetailsScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="MissedFollowupScreen"
-        component={MissedFollowUpsScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="QuestionnaireScreen"
-        component={QuestionnaireScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="MedicalDetails"
-        component={MedicalDetails}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Preview"
-        component={Preview}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  </PatientProvider>
-);
-
-export default function App() {
-  const [isConnected, setIsConnected] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [syncButton, setsyncButton] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = NetInfo.addEventListener(async (state) => {
-      const connected = await checkNetworkConnectivity();
-      setIsConnected(connected);
-
-      if (connected) {
-        Toast.show({
-          type: "success",
-          text1: "Back online",
-        });
-      } else {
-        Toast.show({
-          type: "error",
-          text1: "You are offline now",
-        });
-      }
-    });
-  }, []);
-
-  const handleLoginSuccess = (islogin) => {
-    setIsLoggedIn(islogin);
-  };
-
-  useEffect(() => {
-    const initializeDatabase = async () => {
-      try {
-        // Drop All Tables
-        // await DropService.dropTables();
-        // Initialize database and create tables
-        await CreateService.createTables();
-        console.log("Database and tables initialized successfully.");
-      } catch (error) {
-        console.error("Error initializing database:", error);
-        // Handle the error here, such as showing a message to the user
-        // You can also perform additional actions, such as reattempting initialization or showing an error message
-      }
-    };
-
-    initializeDatabase();
-    // Clean up function to close the database connection
-    return () => {
-      db.closeSync(); // Close the database connection
-    };
-  }, []); // Empty dependency array to ensure this effect runs only once on component mount
-
+import 'react-native-gesture-handler';
+import { StyleSheet} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthProvider } from './src/context/AuthContext';
+import AppNav from './src/navigation/AppNav';
+import { LanguageProvider } from './src/context/LanguageProvider';
+import { NativeWindStyleSheet } from "nativewind";
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+const App =()=>{
+  // verifyInstallation();
   return (
-    <LanguageProvider>
-      <NavigationContainer>
-        {!isLoggedIn ? (
-          <Stack.Navigator>
-            {!isLoggedIn ? (
-              <Stack.Screen name="Login" options={{ headerShown: false }}>
-                {(props) => (
-                  <LoginScreen {...props} onLoginSuccess={handleLoginSuccess} />
-                )}
-              </Stack.Screen>
-            ) : (
-              <Stack.Screen name="Home" component={HomeScreen} />
-            )}
-          </Stack.Navigator>
-        ) : (
-          <MainDrawerNavigator />
-        )}
-        <ForwardedToast />
-      </NavigationContainer>
+    <AuthProvider>
+      <LanguageProvider>
+      <AppNav/>
     </LanguageProvider>
+    </AuthProvider>
   );
 }
 
+// const Home = ()=>{
+//   return(
+//     <SafeAreaView style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+//       <View>
+//         <Text>Home</Text>
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+
 });
+
+export default App;
