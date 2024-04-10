1cd881587d0595c2e0b31cc76118922d8f74da8d
ankushpatil0125 <155605884+ankushpatil0125@users.noreply.github.com>
2024-04-10 23:50:55 +0530

    Profile Screen of Worker
    Profile Screen of Worker

--- HealthWorker/src/screens/ChangePasswordScreen.js
+++ HealthWorker/src/screens/ChangePasswordScreen.js
@@ -65,41 +65,156 @@ const ChangePasswordScreen = ({navigation}) => {
     }
   };
 
-//   return (
-//     <KeyboardAvoidingView
-//       behavior={Platform.OS === "ios" ? "padding" : "height"}
-//       style={{
-//         flex: 1,
-//         backgroundColor: "white",
-//         alignItems: "center",
-//         justifyContent: "center",
-//       }}
-//     >
-//       <ScrollView
-//         contentContainerStyle={{
-//           flexGrow: 1,
-//           paddingHorizontal: 30,
-//           paddingTop: 20,
-//           paddingBottom: 30,
-//           justifyContent: "center",
-//         }}
-//       >
-//         {/* Language Toggle Button */}
-//         <View
-//           style={{ padding: 20, borderTopWidth: 1, borderTopColor: "#ccc",justifyContent: "center",alignItems:'center' }}
-//         >
-//           <LanguageToggleButton
-//             onPress={handleLanguageToggle}
-//             selectedLanguage={selectedLanguage}
-//           />
-//         </View>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingHorizontal: 30,
+          paddingTop: 20,
+          paddingBottom: 30,
+          justifyContent: "center",
+        }}
+      >
+        {/* Language Toggle Button */}
+        <View
+          style={{ padding: 20, borderTopWidth: 1, borderTopColor: "#ccc",justifyContent: "center",alignItems:'center' }}
+        >
+          <LanguageToggleButton
+            onPress={handleLanguageToggle}
+            selectedLanguage={selectedLanguage}
+          />
+        </View>
 
-//         {/* Login to Your Account */}
-//         <View style={{ alignItems: "center" }}>
-//           <Text style={{ fontSize: 17, fontWeight: "bold", color: "black" }}>
-//             {i18n.t("Change Password")}
-//           </Text>
-//         </View>
+        {/* Login to Your Account */}
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 17, fontWeight: "bold", color: "black" }}>
+            {i18n.t("Change Password")}
+          </Text>
+        </View>
+
+        {/* Email */}
+        <View style={{ marginTop: 20 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <AntDesign
+              style={{ marginLeft: 10 }}
+              name="lock"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              value={requestData.oldPassword}
+              onChangeText={(text) => setRequestData({...requestData, oldPassword: text})}
+              secureTextEntry={true}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 280,
+                fontSize: 16,
+              }}
+              placeholder={i18n.t("Enter Your Old Password")}
+            />
+          </View>
+        </View>
+
+        {/* Password */}
+        <View style={{ marginTop: 20 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <AntDesign
+              style={{ marginLeft: 10 }}
+              name="lock"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              value={requestData.newPassword}
+              onChangeText={(text) => setRequestData({...requestData, newPassword: text})}
+              secureTextEntry={true}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 280,
+                fontSize: 16,
+              }}
+              placeholder={i18n.t("Enter Your New Password")}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginTop: 80 }}>
+          <Pressable
+            onPress={handleLogout}
+            style={{
+              width: 200,
+              backgroundColor: "#87CEEB",
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+              padding: 15,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "black",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              {i18n.t("Change Password")}
+            </Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default ChangePasswordScreen;
+
+const styles = StyleSheet.create({});
+
+
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// const ChangePasswordScreen = () => {
+//   return (
+//     <View>
+//       <Text>ChangePasswordScreen</Text>
+//     </View>
+//   )
+// }
+
+// export default ChangePasswordScreen
 
         {/* Email */}
         <View style={{ marginTop: 20 }}>
